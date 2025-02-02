be4dae94a9b0c29930d406ef8230c66ff74d704c
tylermeyers351 <tylermeyers351@gmail.com>
2025-02-02 15:06:59 -0800

    Spawn and move ground

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,13 +6,36 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] int startingChunksAmount = 12;
     [SerializeField] Transform chunkParent;
     [SerializeField] float chunkLength = 10f;
+    [SerializeField] float moveSpeed = 8f;
+
+    GameObject[] chunks =  new GameObject[12];
 
     void Start()
+    {
+        SpawnChunks();
+    }
+
+    void Update()
+    {
+        MoveChunks();
+    }
+
+    void SpawnChunks()
     {
         for (int i = 0; i < startingChunksAmount; i++)
         {
             Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + (chunkLength * i));
-            Instantiate(chunkPrefab, newPosition, Quaternion.identity, chunkParent);
+            GameObject newChunk = Instantiate(chunkPrefab, newPosition, Quaternion.identity, chunkParent);
+
+            chunks[i] = newChunk;
+        }
+    }
+
+    void MoveChunks()
+    {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            chunks[i].transform.Translate(0, 0, moveSpeed * Time.deltaTime * -1);
         }
     }
 }
