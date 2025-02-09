12890f04eea131f50027381e2388d9dea6400a1e
tylermeyers351 <tylermeyers351@gmail.com>
2025-02-08 16:43:23 -0800

    Player Input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 movement;
+    Rigidbody rb;
+    
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float xClamp = 4f;
+    [SerializeField] float zClamp = 2f;
+
+
+    void Awake() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() 
+    {
+        Vector3 position = rb.position;
+
+        
+
+        position.x = position.x + movement.x * moveSpeed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -xClamp, xClamp);
+
+        position.z = position.z + movement.y * moveSpeed * Time.deltaTime;
+        position.z = Mathf.Clamp(position.z, -zClamp, zClamp);
+
+        rb.MovePosition(position);
+    }
+
+    void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+}
